66785f75129e2e8644d932d90dce128a7081115b
lbedoyas <lucho.bedoya1@hotmail.com>
2019-01-03 11:27:03 -0500

    Se crean Modelos album y artist
    Se crean modelos artist y album y se referencia el uno al otro con Shema,ObjectId

--- Models/artist.js
+++ Models/artist.js
@@ -5,18 +5,15 @@ var mongoose = require('mongoose');
 //esta variable es para definir un objeto para guardarlos concretamente en la bd
 var Schema = mongoose.Schema;
 
-var userSchema = Schema({
+var ArtistSchema = Schema({
     name: String,
-    surname: String,
-    email: String,
-    password: String,
-    role: String,
+    description: String,
     image: String
 });
 
-//User en la instruccion module.exports = mongoose.model('User', userSchema); permite el objeto 
+//User en la instruccion module.exports = mongoose.model('Artist', userSchema); permite el objeto 
 //User que es el mismo nombre de la collecion que se tiene en la base de datos de mongodb
 //se guarde como un objeto y se pueda utilizar en otro fichero para que se pueda instanciar
-//de acuerdo a los esquemasdefinidos en la variable userShema 03/01/2019
-module.exports = mongoose.model('User', userSchema);
+//de acuerdo a los esquemasdefinidos en la variable ArtistSchema 03/01/2019
+module.exports = mongoose.model('Artist', ArtistSchema);
 
