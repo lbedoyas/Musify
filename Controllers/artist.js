e602864d98b3fd99837d18636f1293236a2e3678
lbedoyas <lucho.bedoya1@hotmail.com>
2019-01-31 20:13:27 -0500

    Funcion Actualizar artista
    Se codifica la funcion updateArtist con la y se hace prueba con postman para confirmar si efectivamente se efectua los cambios en mongodb

--- Controllers/artist.js
+++ Controllers/artist.js
@@ -86,10 +86,28 @@ function getArtists(req, res){
 
 }
 
+function updateArtist(req,res){
+    var artistId = req.params.id;
+    var update = req.body;
+    Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated) => {
+        if(err){
+            res.status(500).send({message: 'Error al guardar el artista'});
+        }
+        else{
+            if(!artistUpdated){
+                res.status(404).send({message: 'El artista no a sido actualizado'});
+            }else{
+                res.status(200).send({artist: artistUpdated});
+            }
+        }
+    });
+}
+
 
 
 module.exports = {
     getArtist,
     saveArtist,
-    getArtists
+    getArtists,
+    updateArtist
 };
