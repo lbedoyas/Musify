cb7548823451d081dc1676cf634f9fa7e0ac4e6e
lbedoyas <lucho.bedoya1@hotmail.com>
2019-04-15 10:24:21 -0500

    configuracion de cabeceras para control de access
    configuracion de cabeceras para control de access

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // Configurar Cabeceras http
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Authorization,X-API-KEY,Origin,X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Method', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
+
+    next();
+});
+
+
+
 
 
 //Rutas Base
